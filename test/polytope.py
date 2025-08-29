daae2e893f3345bcbaf5e7c4349c957eade8bfdc
Hana <hana.zupan@hotmail.com>
2025-08-29 11:48:54 +0200

    introduced tests

---
+++ test/polytope.py
@@ -0,0 +1,15 @@
+
+from molgri.polytope import IcosahedronPolytope
+
+def test_number_of_nodes():
+    """
+    This applies to polytopes with full number of points. We test that after each level of division, the number of
+    points is as expected.
+    """
+    # ico tests
+
+def test_number_of_neigbours():
+    pass
+
+def test_removing_points():
+    pass
